b91060093e0ce2f5b2eee9baf3f2bc914e2aedf4
ForgetfulWasAria <aria_forgetful@outlook.com>
2021-08-15 06:38:57 -0400

    Moved existing scripts that interact with Windows to Powershell.

--- bin/merge_pdf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Just merges existing pdfs 
-
-pdfunite $(ls *.pdf | sort --numeric | paste -s -d" ") merged.pdf
--- bin/update_win.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-wget --output-document ~/temp/rclone.zip https://downloads.rclone.org/rclone-current-windows-amd64.zip
-
-7z e -y ~/temp/rclone.zip -o/mnt/c/Users/aria_/Apps/rclone
-
-cp /mnt/c/Users/aria_/Apps/rclone/rclone.exe /mnt/c/Users/aria_/Apps/bin
